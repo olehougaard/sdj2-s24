c9e4f54cfd55f823b2c4a7bb256742700d854e17
OIH <OIH@VIA.DK>
2024-02-08 07:28:51 +0100

    After session 1

--- session1/src/dk/via/cashier/CashRegister.java
+++ session1/src/dk/via/cashier/CashRegister.java
@@ -1,32 +1,40 @@
 package dk.via.cashier;
 
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 public class CashRegister {
     // Define the support
     private double total;
+    private final PropertyChangeSupport support;
 
     public CashRegister() {
         total = 0;
         // Initialize the support
+        support = new PropertyChangeSupport(this);
     }
 
     public void completeSale() {
         double oldTotal = total;
         total = 0;
         // Fire the event
+        support.firePropertyChange("total", oldTotal, total);
     }
 
     public void registerSale(double amount) {
+        double oldTotal = total;
         total += amount;
         // Fire the event
+        support.firePropertyChange("total", oldTotal, total);
     }
 
     public void addPropertyChangeListener(PropertyChangeListener listener) {
       // Add the listener
+        support.addPropertyChangeListener(listener);
     }
 
     public void removePropertyChangeListener(PropertyChangeListener listener) {
       // Remove the listener
+        support.removePropertyChangeListener(listener);
     }
 }
